d3449597ec94102f75095ed295688b1cb0fbaabd
Lucas <lucas75maia@gmail.com>
2023-11-03 15:05:49 -0300

    update

--- src/app/components/Menus.tsx
+++ src/app/components/Menus.tsx
@@ -55,8 +55,8 @@ export default function Menus({ videoRefs }: { videoRefs: any }) {
             <p className="text-lg font-semibold leading-7 text-gray-900">
               Modelo - {item.title}
             </p>
-            <div className="lg:flex-shrink-0">
-              <svg viewBox="0 0 366 729" role="img" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
+            <div className="flex relative justify-center items-center">
+              {/* <svg viewBox="0 0 366 729" role="img" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
                 <defs>
                   <clipPath id="2ade4387-9c63-4fc4-b754-10e687a0d332">
                     <rect width={316} height={684} rx={36} />
@@ -73,25 +73,56 @@ export default function Menus({ videoRefs }: { videoRefs: any }) {
                 <foreignObject
                   width={316}
                   height={684}
-                  className="flex justify-center items-center"
                   transform="translate(24 24)"
                   clipPath="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)"
                 >
-                  {/* <video
+                  <video
                     ref={(element) => {
                       if (element) {
                         videoRefs.current[index + 1] = element;
                       }
                     }}
-                    className="w-full h-full rounded-lg shadow-lg"
                     playsInline
-                    loop
+                    width={316}
+                    height={684}
                     autoPlay={true}
                     muted>
                     <source src={item.src} type="video/mp4" />
-                  </video> */}
+                  </video>
                 </foreignObject>
-              </svg>
+              </svg> */}
+              <div className="relative">
+                <div className="relativebg-black">
+                  {/* <svg viewBox="0 0 366 729" role="img" className="absolute mx-auto min-h-full w-[105%] -top-2 -z-10 -left-[8px]">
+                    <path
+                      fill="#4B5563"
+                      d="M363.315 64.213C363.315 22.99 341.312 1 300.092 1H66.751C25.53 1 3.528 22.99 3.528 64.213v44.68l-.857.143A2 2 0 0 0 1 111.009v24.611a2 2 0 0 0 1.671 1.973l.95.158a2.26 2.26 0 0 1-.093.236v26.173c.212.1.398.296.541.643l-1.398.233A2 2 0 0 0 1 167.009v47.611a2 2 0 0 0 1.671 1.973l1.368.228c-.139.319-.314.533-.511.653v16.637c.221.104.414.313.56.689l-1.417.236A2 2 0 0 0 1 237.009v47.611a2 2 0 0 0 1.671 1.973l1.347.225c-.135.294-.302.493-.49.607v377.681c0 41.213 22 63.208 63.223 63.208h95.074c.947-.504 2.717-.843 4.745-.843l.141.001h.194l.086-.001 33.704.005c1.849.043 3.442.37 4.323.838h95.074c41.222 0 63.223-21.999 63.223-63.212v-394.63c-.259-.275-.48-.796-.63-1.47l-.011-.133 1.655-.276A2 2 0 0 0 366 266.62v-77.611a2 2 0 0 0-1.671-1.973l-1.712-.285c.148-.839.396-1.491.698-1.811V64.213Z"
+                    />
+                  </svg> */}
+                  <div className="-z-10 rounded-sm bg-[#4b5563] absolute top-12 -left-1 w-5 h-7" />
+                  <div className="-z-10 rounded-sm bg-[#4b5563] absolute top-24 -left-1 w-5 h-16" />
+                  <div className="-z-10 rounded-sm bg-[#4b5563] absolute top-44 -left-1 w-5 h-16" />
+                  <div className="-z-10 rounded-sm bg-[#4b5563] absolute top-44 -right-1 w-5 h-16" />
+                  <div className="overflow-hidden border-[1rem] rounded-3xl border-[#4b5563]">
+                    <div className="z-10 bg-[#343e4e] overflow-hidden rounded-sm border-[.4rem] border-[#343e4e]">
+                      <video
+                        ref={(element) => {
+                          if (element) {
+                            videoRefs.current[index + 1] = element;
+                          }
+                        }}
+                        playsInline
+                        width={316}
+                        height={684}
+                        className="rounded-xl"
+                        autoPlay={true}
+                        muted>
+                        <source src={item.src} type="video/mp4" />
+                      </video>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         ))}
